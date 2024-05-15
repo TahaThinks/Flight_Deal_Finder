174674116f0ab646f1b32a702430e43fc83e0e44
TahaThinks <tahaa4587@gmail.com>
2024-05-15 11:52:37 +0400

    from main call getdata

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+from flight_data import FlightData
+
+flight_details = FlightData()
+flight_details.getdata()
