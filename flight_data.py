cef683c1e70b5bddbf4b9e8bbdaa971d249aa63b
TahaThinks <tahaa4587@gmail.com>
2024-05-17 12:33:52 +0400

    Get period six month

--- flight_data.py
+++ flight_data.py
@@ -1,7 +1,13 @@
+import datetime
+
 from api_data import SHEETY_AUTH, SHEETY_URL
 import requests
 import datetime as dt
 
+tomorrow = dt.datetime.now() + datetime.timedelta(days=1)
+six_months = dt.datetime.now() + datetime.timedelta(weeks=6*4)
+print(tomorrow.strftime("%Y-%m-%d"))
+print(six_months.strftime("%Y-%m-%d"))
 
 class FlightData:
     # This class is responsible for structuring the flight data.
