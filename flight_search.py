96fb9acc91f1157b6c0aed4d49522190cd2193e4
TahaThinks <tahaa4587@gmail.com>
2024-05-21 09:18:16 +0400

    Refactor

--- flight_search.py
+++ flight_search.py
@@ -1,7 +1,19 @@
 from api_data import AMADEUS_KEY, AMADEUS_SECRET, AMADEUS_URL
+import requests
 class FlightSearch:
     # This class is responsible for talking to the Flight Search API.
     def write_iataCode(self, flight_available):
+        city = flight_available["city"]
+        amadeus_endpoint = f"{AMADEUS_URL}/reference-data/locations"
+        amadeus_params = {
+            "subType": "CITY",
+            "keyword": city,
+        }
+        amadeus_header = {
+            "Authorization": f"Bearer {AMADEUS_SECRET}"
+        }
+        amadeus_response = requests.get(url=amadeus_endpoint, params=amadeus_params, headers=amadeus_header)
+        print(amadeus_response.json())
 
         flight_available["iataCode"] = "TESTING"
         return flight_available
